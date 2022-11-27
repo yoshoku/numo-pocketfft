fa78c989fc446e0b3dd3e99ad7ca385de4084024
yoshoku <yoshoku@outlook.com>
2022-11-27 21:21:19 +0900

    fix: add dynamic_lookup linker option for macOS and Ruby 3.1

--- ext/numo/pocketfft/extconf.rb
+++ ext/numo/pocketfft/extconf.rb
@@ -26,6 +26,12 @@ if RUBY_PLATFORM =~ /mswin|cygwin|mingw/
   end
 end
 
+if RUBY_PLATFORM.match?(/darwin/) && Gem::Version.new('3.1.0') <= Gem::Version.new(RUBY_VERSION)
+  if try_link('int main(void){return 0;}', '-Wl,-undefined,dynamic_lookup')
+    $LDFLAGS << ' -Wl,-undefined,dynamic_lookup'
+  end
+end
+
 $CFLAGS = "#{$CFLAGS} -std=c99"
 
 $srcs = Dir.glob("#{$srcdir}/**/*.c").map { |path| File.basename(path) }
